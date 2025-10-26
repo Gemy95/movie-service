dba45dc9053568fd323a335f17160082fc808aec
ali.gamal <ali.gamal95880@gmail.com>
2025-10-26 15:35:44 +0300

    feat/movie-service-config

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -2,6 +2,7 @@ const DEFAULT_SERVER_PORT = 5000;
 
 export interface Configuration {
   app: AppSetting;
+  services: Services;
 }
 
 export interface AppSetting {
@@ -9,11 +10,25 @@ export interface AppSetting {
   port: number;
 }
 
+export interface Movie {
+  url: string;
+  apiKey: string;
+}
+export interface Services {
+  movie: Movie;
+}
+
 export const configuration = (): Configuration => {
   const defaultConfiguration: Configuration = {
     app: {
       env: process.env.NODE_ENV,
       port: parseInt(process.env.SERVER_PORT, 10) || DEFAULT_SERVER_PORT,
+    },
+    services: {
+      movie: {
+        url: process.env.MOVIE_BASE_URL,
+        apiKey: process.env.MOVIE_API_KEY,
+      },
     },
   };
   return defaultConfiguration;
