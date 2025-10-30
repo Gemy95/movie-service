b34819bcdc74f794ff804367fd4924b57777a203
ali.gamal <ali.gamal95880@gmail.com>
2025-10-30 10:52:55 +0300

    feat/remove-files

--- src/shared/interfaces/helpers/generate-pagination.helper.ts
+++
@@ -1,5 +0,0 @@
-export const generatePagination = (page?: number, perPage?: number) => {
-  const skip = !isNaN(page) && !isNaN(perPage) ? (page - 1) * perPage : undefined;
-  const take = !isNaN(page) && !isNaN(perPage) ? perPage : undefined;
-  return { skip, take };
-};
